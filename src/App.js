c71d49df7f95b085db2112d04061911b5e41cb2e
jai-sharma <sendtojsharma@gmail.com>
2020-09-28 10:47:50 +0530

    added form and todolist component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.scss';
 
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
 function App() {
     return (
         <div className="App">
-
             <header>
-                <h1>Jay's Todo List</h1>
+                <h1>what're you upto today?</h1>
             </header>
 
+            <Form />
+
+            <TodoList />
         </div>
     );
 }
