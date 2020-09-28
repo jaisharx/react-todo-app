da620219233e8b843fae0948a3833ebbd4beff20
jai-sharma <sendtojsharma@gmail.com>
2020-09-28 10:48:20 +0530

    working on components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { FaPlusSquare } from 'react-icons/fa';
 
 const Form = () => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <form>
+            <input type="text" className="todo-input" />
+            <button className="todo-button" type="submit">
+                <FaPlusSquare />
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+};
 
-export default Form
+export default Form;
